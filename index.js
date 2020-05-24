f49cec6aae08ed4a92652f7f9ef9c0018e6583ea
ApocalypseCalculator <>
2020-05-23 22:09:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var lastpingerid = '';
+var reconnectcount = 0;
 
 const token = require('./token'); //you can replace the require(./token) with the token string if you want
 const prefix = 'someone!'; //you can change this if you like
@@ -31,39 +32,51 @@ client.on('message', msg => {
   else if (msg.isMentioned(client.user.id) && msg.author.id === lastpingerid) {
     msg.reply('calm down with the pings dude');
   }
-  else if (msg.isMentioned(client.user.id) && !msg.content.includes('\<@')) {
-    msg.guild.fetchMember(client.user).then(member => {
-      if (member.hasPermission('ADMINISTRATOR') || (member.hasPermission('MANAGE_WEBHOOKS') && member.hasPermission('MANAGE_MESSAGES'))) {
-        msg.channel.createWebhook(msg.member.displayName, msg.author.avatarURL).then(webhook => {
-          console.log('pinger: ' + msg.author.username + '(' + msg.author.id + ')\t content: ' + msg.content.replace('<@!' + client.user.id + '>', '(botping)'));
-          if(msg.content.includes('<@!' + client.user.id + '>')){
-            webhook.send(msg.content.replace('<@!' + client.user.id + '>', '<@!' + getrandomuserid(msg) + '>'));
-          }
-          else{
-            webhook.send(msg.content.replace('<@' + client.user.id + '>', '<@!' + getrandomuserid(msg) + '>'));
-          }
-          webhook.delete();
-          msg.delete(0);
-          lastpingerid = msg.author.id;
-        }).catch(error => {
-          console.log(error);
-          msg.channel.send('There was an error with performing the random ping. This is usually caused by missing permissions. Please grant me either admin or manage webhook + manage messages permissions for this channel. You can contact <@492079026089885708> if this problem persists');
-        });
+  else if (msg.isMentioned(client.user.id) && !msg.content.includes("\\<@")) {
+    if (msg.content.includes('@everyone') || msg.content.includes('@here') || msg.mentions.roles.size > 0) {
+      if (!msg.member.hasPermission('MENTION_EVERYONE')) {
+        msg.channel.send("I see what you're doing, and I don't like it");
+        console.log('attempted ping by: ' + msg.author.username + '\tcontent:\t' + msg.content);
       }
       else {
-        msg.channel.send('Insufficient permissions. Please either grant me admin or give me both manage webhooks and manage messages');
-        const embed = new Discord.RichEmbed()
-          .setColor(13833)
-          .setAuthor(client.user.username, client.user.avatarURL)
-          .setTitle('Permissions Demo')
-          .setImage('https://cdn.discordapp.com/attachments/711370772114833520/711620022669148180/demo3.gif')
-          .setTimestamp()
-          .setFooter("Someone Bot By ApocalypseCalculator - Under MIT License", client.user.avatarURL);
-        msg.channel.send(embed);
+        msg.channel.send("I'm going to give everyone 1 less ping by not repeating that with a Someone ping. tyvm");
+        console.log('attempted ping by: ' + msg.author.username + '\tcontent:\t' + msg.content);
       }
-    })
+    }
+    else {
+      msg.guild.fetchMember(client.user).then(member => {
+        if (member.hasPermission('ADMINISTRATOR') || (member.hasPermission('MANAGE_WEBHOOKS') && member.hasPermission('MANAGE_MESSAGES'))) {
+          msg.channel.createWebhook(msg.member.displayName, msg.author.avatarURL).then(webhook => {
+            console.log('pinger: ' + msg.author.username + '(' + msg.author.id + ')\t content: ' + msg.content.replace('<@!' + client.user.id + '>', '(botping)'));
+            if (msg.content.includes('<@!' + client.user.id + '>')) {
+              webhook.send(msg.content.replace('<@!' + client.user.id + '>', '<@!' + getrandomuserid(msg) + '>'));
+            }
+            else {
+              webhook.send(msg.content.replace('<@' + client.user.id + '>', '<@!' + getrandomuserid(msg) + '>'));
+            }
+            webhook.delete();
+            msg.delete(0);
+            lastpingerid = msg.author.id;
+          }).catch(error => {
+            console.log(error);
+            msg.channel.send('There was an error with performing the random ping. This is usually caused by missing permissions. Please grant me either admin or manage webhook + manage messages permissions for this channel. You can contact <@492079026089885708> if this problem persists');
+          });
+        }
+        else {
+          msg.channel.send('Insufficient permissions. Please either grant me admin or give me both manage webhooks and manage messages');
+          const embed = new Discord.RichEmbed()
+            .setColor(13833)
+            .setAuthor(client.user.username, client.user.avatarURL)
+            .setTitle('Permissions Demo')
+            .setImage('https://cdn.discordapp.com/attachments/711370772114833520/711620022669148180/demo3.gif')
+            .setTimestamp()
+            .setFooter("Someone Bot By ApocalypseCalculator - Under MIT License", client.user.avatarURL);
+          msg.channel.send(embed);
+        }
+      })
+    }
   }
-  else if(msg.isMentioned(client.user.id)){
+  else if (msg.isMentioned(client.user.id)) {
     msg.channel.send("I see what you are doing, and I don't like it");
     console.log(msg.author.username + '\t(failed ping)\t: ' + msg.content);
   }
@@ -95,6 +108,7 @@ client.on('message', msg => {
       .addField('Ping Command', prefix + 'ping', true)
       .addField('Webhook Clearing Command', prefix + 'webhookclear', true)
       .addField('Troll Command', prefix + 'help', true)
+      .addField('Reconnect Count Since Last Reboot', prefix + 'discordbad', true)
       .addField('Experimental Ping Contest Command', prefix + 'pingcontest (not available yet)', true)
       .addBlankField()
       .addField('Server Count', client.guilds.size, true)
@@ -106,6 +120,9 @@ client.on('message', msg => {
       .setTimestamp()
       .setFooter("Someone Bot By ApocalypseCalculator - Under MIT License", client.user.avatarURL);
     msg.channel.send(msgembed);
+  }
+  else if (msg.content === prefix + 'discordbad') {
+    msg.channel.send('Discord API is fucking trash. I had to reconnect ' + reconnectcount + ' times since my last reboot because Discord is fucking stoopid');
   }
 })
 
@@ -124,5 +141,17 @@ function getrandomuserid(msg) {
   console.log('returned id: ' + id + '\t server: ' + msg.guild.id);
   return id;
 }
+
+client.on('reconnecting', () => {
+  reconnectcount++;
+});
+
+client.on('disconnect', () => {
+  console.log('bot disconnected. Offline');
+});
+
+client.on('error', error => {
+  console.log(error);
+});
 
 client.login(token);
