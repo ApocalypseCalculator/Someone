2329392ceaeb533594545436c8371ff57f533ba6
ApocalypseCalculator <>
2020-06-21 11:28:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ client.on('message', msg => {
             .setTitle('Permissions Demo')
             .setImage('https://cdn.discordapp.com/attachments/711370772114833520/711620022669148180/demo3.gif')
             .setTimestamp()
-            .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+            .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
           msg.channel.send(embed);
         }
       })
@@ -128,7 +128,7 @@ client.on('message', msg => {
       .addField('Privacy Policy', prefix + 'privacy', true)
       .addBlankField()
       .setTimestamp()
-      .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+      .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
     msg.channel.send(msgembed);
   }
   else if (msg.content === prefix + 'discordbad') {
@@ -140,7 +140,7 @@ client.on('message', msg => {
       .setAuthor(client.user.username, client.user.avatarURL)
       .addBlankField()
       .setTimestamp()
-      .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+      .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
     if (msg.mentions.members.size > 1) {
       msg.channel.send('Bro please mention one user you want to check pings for');
     }
@@ -183,7 +183,7 @@ client.on('message', msg => {
       .setDescription('The following are the first 10 people on the leaderboard')
       .addBlankField()
       .setTimestamp()
-      .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+      .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
     let rawdata = fs.readFileSync('globalLeaderboard.json');
     let parsed = JSON.parse(rawdata);
     let list = parsed.users;
@@ -214,7 +214,7 @@ client.on('message', msg => {
       .addField('Privacy Policy of Someone bot', 'By adding Someone Bot™️ to your server, you agree to having your Discord snowflakes collected by us and retained indefinitely. We collect this data in order to store information on how many times a user has been pinged through this bot. This privacy policy can change without notice, and we encourage you to check it regularly. If you do not agree with this policy, please promptly kick Someone Bot from your server.')
       .addBlankField()
       .setTimestamp()
-      .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+      .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
     msg.channel.send(msgembed);
   }
   else if (msg.content.startsWith(prefix + 'grank')) {
@@ -229,7 +229,7 @@ client.on('message', msg => {
       .setDescription("Shows your global rank, to show someone else's rank, append a ping to the command")
       .addBlankField()
       .setTimestamp()
-      .setFooter("Someone Bot By ApocalypseCalculator - Under GNU General Public License", client.user.avatarURL);
+      .setFooter("Someone Bot By ApocalypseCalculator - Under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", client.user.avatarURL);
     if (msg.mentions.members.size > 1) {
       msg.channel.send('Bro please mention one user you want to check pings for');
     }
