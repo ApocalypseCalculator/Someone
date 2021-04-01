0521fa7bf94702bb05b3c4256efc554cbe51d16e
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-01 12:21:44 -0400

    add hostid in config

--- data/config.js
+++ data/config.js
@@ -2,5 +2,6 @@ module.exports = {
     prefix: "someone!",
     pingcooldown: 60000, //1 minute
     creatorid: '492079026089885708', //you may not change this line!
+    hostid: '492079026089885708', //you can change this to your own ID to access hoster commands
     logging: true //turn to false for off, true for on
 }
