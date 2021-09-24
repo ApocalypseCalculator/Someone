953d51309c78d77a3188d0cc7a1dbe49b8ab20dc
Terrarian <72149678+Terra-rian@users.noreply.github.com>
2021-09-24 13:46:10 -0400

    feat(events): imagine me forgetting to load events smh

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { Client, Collection } from 'discord.js';
 import { token } from './assets/token';
-import { Command, SlashCommand } from './typings/bot';
+import { Command, EventHandler, SlashCommand } from './typings/bot';
 
 export class Someone extends Client {
     commands: Collection<string, Command>;
@@ -10,7 +10,7 @@ export class Someone extends Client {
 
     constructor() {
         super({
-            intents: 2675, // most guild-related non-privileged + the GUILD_MEMBERS intent
+            intents: 2675, // most guild-related non-privileged intents + the GUILD_MEMBERS intent
         });
 
         this.commands = new Collection();
@@ -22,14 +22,24 @@ const client = new Someone();
 
 fs.readdirSync(path.join(process.cwd(), 'src', 'commands')).forEach((file) => {
     const command: Command = require(path.join(process.cwd(), 'src', 'commands', `${file}`));
-    if (command.name == null || command.execute == null) {
+    if(!command.name || !command.execute) {
         console.error(`\x1b[31mInvalid command: ${file}\x1b[0m`);
-    } else if (command.name in client.commands) {
+    } else if(command.name in client.commands) {
         console.error(`\x1b[31mDuplicate command name: ${file} (${command.name})\x1b[0m`);
     } else {
         client.commands.set(command.name, command);
         console.log(`Loaded command: ${file} (${command.name})`);
     }
 });
+
+const events = fs.readdirSync(path.join(process.cwd(), 'src', 'events'));
+function loadEvents() {
+    for(const file of events) {
+        const event: EventHandler = require(path.join(process.cwd(), 'src', 'events', file));
+        client.on(event.name, event.callback.bind(client));
+    }
+}
+
+loadEvents();
 
 client.login(token);
