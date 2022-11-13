cc81dc03f662c644ab5fe0161af1de003db6a185
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-13 17:26:16 -0500

    switch fake command

--- src/slashcommands/fake.ts
+++ src/slashcommands/fake.ts
@@ -1,6 +1,6 @@
 import { ApplicationCommandOptionType, TextChannel } from 'discord.js';
 import { config } from '../assets/config';
-import { getRandomUserID } from '../assets/functions';
+import { getRandomUserID, sendWebhook } from '../assets/functions';
 import { SlashCommand } from '../typings/bot';
 
 export = {
@@ -15,37 +15,30 @@ export = {
     }],
     execute: async (interaction) => {
         const message = interaction.options.get('message', true).value;
-        if(typeof message !== 'string') {
+        if (typeof message !== 'string') {
             return;
         }
 
-        if(/@everyone/.test(message) || /<@!?&[0,9]{18}>/.test(message)) {
+        if (/@everyone/.test(message) || /<@!?&[0,9]{18}>/.test(message)) {
             return interaction.reply('Ahem I will not ping in a fake message');
         } else {
             const fakemember = await getRandomUserID(interaction);
-            const faker = interaction.guild?.members.cache.get(fakemember);
+            const faker = interaction.guild?.members.cache.get(fakemember)?.user;
 
             try {
-                if(!(interaction.channel instanceof TextChannel) || !faker) {
+                if (!(interaction.channel instanceof TextChannel) || !faker) {
                     return;
                 }
 
-                const webhook = await interaction.channel.createWebhook({
-                    name: faker.displayName,
-                    avatar: faker.user.avatarURL(),
-                    reason: `Fake message requested by ${interaction.user.tag} (${interaction.user.id})`,
-                });
+                let success = await sendWebhook(interaction, faker, message)
 
-                webhook.send(message).then((msg) => {
-                    console.log(`fake message for ${faker.id} created by ${interaction.user.id}. Link suffix is ${msg.url.slice('https://discordapp.com/channels/'.length)}`);
-                });
-
-                setTimeout(() => {
-                    return webhook.delete();
-                }, 1000);
-
-                return interaction.reply({ content: 'Your fake message was sent!', ephemeral: true });
-            } catch(error) {
+                if (success) {
+                    return interaction.reply({ content: 'Your fake message was sent!', ephemeral: true });
+                }
+                else {
+                    return interaction.reply(`There was an error making the fake message. This may be due to missing create webhook permissions.`);
+                }
+            } catch (error) {
                 console.log(error);
                 return interaction.reply(`There was an error with making the fake message. This is usually caused by missing permissions. Please grant me either admin or manage webhook + manage messages permissions for this channel. You can contact ApocalypseCalculator <@${config.creatorID}> if this problem persists`);
             }
