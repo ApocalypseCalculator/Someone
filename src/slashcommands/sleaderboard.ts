a4c796dbf0e3ee8c3982edb12f8c7c4bb813d278
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-13 17:58:14 -0500

    sleaderboard

--- src/slashcommands/sleaderboard.ts
+++ src/slashcommands/sleaderboard.ts
@@ -1,15 +1,14 @@
-import fs from 'fs';
-import path from 'path';
 import { config } from '../assets/config';
 import { EmbedBuilder } from 'discord.js';
 import { SlashCommand } from '../typings/bot';
-import { GlobalLeaderboardTotalData } from '../typings/assets';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export = {
     name: 'sleaderboard',
     description: 'Shows the server leaderboard for pings.',
     global: true,
-    execute: (interaction, client) => {
+    execute: async (interaction, client) => {
         const embed = new EmbedBuilder()
             .setColor(13833)
             .setAuthor({ name: client?.user?.username ?? '', iconURL: client?.user?.avatarURL() ?? '' })
@@ -19,20 +18,23 @@ export = {
             .setTimestamp()
             .setFooter({ text: 'Someone Bot By ApocalypseCalculator - Licensed', iconURL: client?.user?.avatarURL() ?? '' });
 
-        const rawData = fs.readFileSync(path.join(process.cwd(), 'src', 'data', 'globalLeaderboard.json'), { encoding: 'utf-8' });
-        const parsed: GlobalLeaderboardTotalData = JSON.parse(rawData);
-
-        const list = parsed.users.filter((user) => interaction.guild?.members.cache.get(user.discordID));
-        list.sort((a, b) => (a.pinged > b.pinged) ? 1 : -1);
+        const querylist = await prisma.user.findMany(
+            {
+                orderBy: {
+                    pinged: 'desc'
+                }
+            }
+        );
+        const list = querylist.filter((user) => interaction.guild?.members.cache.get(user.discordid));
         for(let i = 0; i < ((list.length < 10) ? list.length : 10); i++) {
             if(i === 0) {
-                embed.addFields({ name: `#${(i + 1)}`, value: '🥇<@!' + list[list.length - i - 1].discordID + '> ' + ((list[list.length - i - 1].discordID === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
+                embed.addFields({ name: `#${(i + 1)}`, value: '🥇<@!' + list[list.length - i - 1].discordid + '> ' + ((list[list.length - i - 1].discordid === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
             } else if(i === 1) {
-                embed.addFields({ name: `#${(i + 1)}`, value: '🥈<@!' + list[list.length - i - 1].discordID + '> ' + ((list[list.length - i - 1].discordID === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
+                embed.addFields({ name: `#${(i + 1)}`, value: '🥈<@!' + list[list.length - i - 1].discordid + '> ' + ((list[list.length - i - 1].discordid === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
             } else if(i === 2) {
-                embed.addFields({ name: `#${(i + 1)}`, value: '🥉<@!' + list[list.length - i - 1].discordID + '> ' + ((list[list.length - i - 1].discordID === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
+                embed.addFields({ name: `#${(i + 1)}`, value: '🥉<@!' + list[list.length - i - 1].discordid + '> ' + ((list[list.length - i - 1].discordid === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
             } else {
-                embed.addFields({ name: `#${(i + 1)}`, value: '🏅<@!' + list[list.length - i - 1].discordID + '> ' + ((list[list.length - i - 1].discordID === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
+                embed.addFields({ name: `#${(i + 1)}`, value: '🏅<@!' + list[list.length - i - 1].discordid + '> ' + ((list[list.length - i - 1].discordid === config.creatorID) ? '**(👑 bot creator)**' : '') + ': ' + list[list.length - i - 1].pinged + ' pings' });
             }
         }
 
