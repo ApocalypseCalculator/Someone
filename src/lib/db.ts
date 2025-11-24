9f2949661b254cdabd0f1bac596c12449b800353
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-24 02:00:03 -0500

    use better-sqlite3 adapter for unix epoch timestamps

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,19 @@
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+
+if(!process.env.DATABASE_URL) {
+    console.error("DATABASE_URL environment variable is not set.");
+    process.exit(1);
+}
+
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL
+}, {
+    timestampFormat: 'unixepoch-ms'
+});
+
+const prisma = new PrismaClient({
+    adapter
+});
 
 export default prisma;
