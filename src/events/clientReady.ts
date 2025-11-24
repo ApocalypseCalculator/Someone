e1387c83e3aa09a4b67778eb54189c54f5feee04
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-24 13:45:06 -0500

    better command loading

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -5,53 +5,38 @@ import { config } from '../config';
 
 import { EventHandler, SlashCommand } from '../typings/bot';
 import { Someone } from '..';
-import { ActivityType } from 'discord.js';
+import { ActivityType, ApplicationCommandDataResolvable } from 'discord.js';
 import { markUncachedOnColdStart } from '../lib/membercache';
 
 export = {
 	name: 'clientReady',
 	async callback() {
 		const self = this as unknown as Someone;
-
-		if (process.env.REGISTER_CMDS === "unregister") {
-			await self.application?.commands.set([]);
-			await self.guilds.cache.get(config.mainGuild)?.commands.set([]);
-			console.log('Unregistered all slash commands.');
-			process.exit(0);
-		}
-
 		await markUncachedOnColdStart();
 
 		try {
 			const slash_command_files = fs.readdirSync(path.join(process.cwd(), 'src', 'slashcommands'));
 
-			// todo: non-blocking command loading
+			const commandlist: ApplicationCommandDataResolvable[] = [];
+
 			for (const file of slash_command_files) {
 				const command: SlashCommand = require(path.join(process.cwd(), 'src', 'slashcommands', `${file}`));
 
-				if (process.env.REGISTER_CMDS === "global") {
-					const data = {
-						name: command.name,
-						description: command.description,
-						options: command.options,
-					};
-
-					await self.application?.commands.create(data);
-				} else if (process.env.REGISTER_CMDS === "guild") {
-					const data = {
-						name: command.name,
-						description: command.description,
-						options: command.options,
-					};
-
-					await self.guilds.cache.get(config.mainGuild)?.commands.create(data);
-				}
+				commandlist.push({
+					name: command.name,
+					description: command.description,
+					options: command.options,
+				});
 
 				self.slashcommands.set(command.name, command);
 				console.log(`[Slash Command] ${file} loaded!`);
 			}
-			if (['global', 'guild'].includes(process.env.REGISTER_CMDS || '')) {
-				console.log(`Registered slash commands to ${process.env.REGISTER_CMDS} scope.`);
+			if (process.env.REGISTER_CMDS === "global") {
+				await self.application?.commands.set(commandlist);
+				console.log(`Registered slash commands to global scope.`);
+			} else if (process.env.REGISTER_CMDS === "guild") {
+				await self.application?.commands.set(commandlist, config.mainGuild);
+				console.log(`Registered slash commands to guild scope. (Guild ID: ${config.mainGuild}, Guild Name: ${self.guilds.cache.get(config.mainGuild)?.name})`);
 			}
 		} catch (error) {
 			console.log((error as Error).stack);
