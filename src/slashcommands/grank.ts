50bab1cf6168f34025fd93f513c9dab99257a574
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-23 22:27:53 -0500

    update /grank

--- src/slashcommands/grank.ts
+++ src/slashcommands/grank.ts
@@ -1,6 +1,7 @@
 import { SlashCommand } from '../typings/bot';
-import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { ApplicationCommandOptionType } from 'discord.js';
 import prisma from '../lib/db'
+import createBaseEmbed from '../lib/embed';
 
 export = {
     name: 'grank',
@@ -13,14 +14,12 @@ export = {
         required: true,
     }],
     execute: async (interaction, client) => {
-        const embed = new EmbedBuilder()
-            .setColor(13833)
-            .setAuthor({ name: client?.user?.username ?? '', iconURL: client?.user?.avatarURL() ?? '' })
-            .setTitle('Global Ping Leaderboard Rank Information')
-            .setDescription('Shows your global rank / someone else\'s rank')
-            .addFields({ name: '\u200B', value: '\u200B' })
-            .setTimestamp()
-            .setFooter({ text: 'Someone Bot By ApocalypseCalculator - Licensed', iconURL: client?.user?.avatarURL() ?? '' });
+        await interaction.deferReply();
+        const embed = createBaseEmbed(
+            client,
+            'Global Ping Rank',
+            ''
+        )
 
         let user = await prisma.user.findUnique({
             where: {
@@ -28,20 +27,18 @@ export = {
             }
         })
         if (!user) {
-            embed.addFields({ name: 'Ping Count', value: `<@!${interaction.options.getUser('user', true).id}> is not ranked` });
-            embed.addFields({ name: '\u200B', value: '\u200B' });
-            return interaction.reply({ embeds: [embed] });
+            embed.setDescription(`<@!${interaction.options.getUser('user', true).id}> is not ranked`);
         }
-        let lgtlist = await prisma.user.findMany({
-            where: {
-                pinged: {
-                    gt: user?.pinged
+        else {
+            let rank = await prisma.user.count({
+                where: {
+                    pinged: {
+                        gt: user.pinged
+                    }
                 }
-            }
-        });
-        embed.addFields({ name: 'Ping Count', value: `<@!${interaction.options.getUser('user', true).id}> is ranked **#${lgtlist.length + 1}** globally for pings received` });
-        embed.addFields({ name: '\u200B', value: '\u200B' });
-
-        return interaction.reply({ embeds: [embed] });
+            }) + 1;
+            embed.setDescription(`<@!${interaction.options.getUser('user', true).id}> is ranked **#${rank}** globally for pings received`);
+        }
+        return interaction.followUp({ embeds: [embed] });
     },
 } as SlashCommand;
