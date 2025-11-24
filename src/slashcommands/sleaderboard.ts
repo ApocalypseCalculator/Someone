9dca1917523280e1be39fa326ea86ad3ad15944c
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-23 21:41:23 -0500

    use common leaderboard embed

--- src/slashcommands/sleaderboard.ts
+++ src/slashcommands/sleaderboard.ts
@@ -1,7 +1,7 @@
 import { SlashCommand } from '../typings/bot';
 import { throttledAllMembersFetch } from '../lib/functions';
 import prisma from '../lib/db';
-import createEmbed from '../lib/embed';
+import createBaseEmbed, { createLeaderboardEmbed } from '../lib/embed';
 
 export = {
     name: 'sleaderboard',
@@ -9,10 +9,10 @@ export = {
     global: true,
     execute: async (interaction, client) => {
         await interaction.deferReply();
-        const embed = createEmbed(
+        const embed = createBaseEmbed(
             client, 
             'Server Ping Leaderboard', 
-            `Top 10 @someone ping recipients in ${interaction.guild?.name}`
+            `Top 10 @someone ping recipients in \`${interaction.guild?.name}\``
         );
 
         const querylist = await prisma.user.findMany(
@@ -24,17 +24,8 @@ export = {
         );
         await throttledAllMembersFetch(interaction.guild!);
         const list = querylist.filter((user) => interaction.guild?.members.cache.has(user.discordid));
-        if(list.length === 0) {
-            embed.setDescription('No ranked users found in this server');
-            return interaction.followUp({embeds: [embed]});
-        }
-
-        for(let i = 0; i < ((list.length < 10) ? list.length : 10); i++) {
-            let medal = i === 0 ? '🥇' : i === 1 ? '🥈' : i === 2 ? '🥉' : '🏅';
-            embed.addFields({ name: `#${(i + 1)}`, value: `${medal} <@!${list[i].discordid}> : ${list[i].pinged} pings` });
-        }
-
-        embed.addFields({ name: '\u200B', value: 'Out of ' + list.length + ' ranked users' });
-        return interaction.followUp({embeds: [embed]});
+        return interaction.followUp({
+            embeds: [createLeaderboardEmbed(embed, list)]
+        });
     },
 } as SlashCommand;
