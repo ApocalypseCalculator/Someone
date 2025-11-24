4b20a3976e2ea19a0e4627b0affa562d7fd7e423
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2025-11-23 21:59:09 -0500

    update /invite

--- src/slashcommands/invite.ts
+++ src/slashcommands/invite.ts
@@ -1,19 +1,17 @@
 import { SlashCommand } from '../typings/bot';
-import { EmbedBuilder } from 'discord.js';
+import createBaseEmbed from '../lib/embed';
 
 export = {
     name: 'invite',
     description: 'How to invite the bot.',
     global: true,
     execute: (interaction, client) => {
-        const embed = new EmbedBuilder()
-            .setColor(13833)
-            .setAuthor({ name: client?.user?.username ?? '', iconURL: client?.user?.avatarURL() ?? '' })
-            .setTitle('Invite Me To Your Server!')
-            .addFields({ name: 'Invite Link', value: '[Click Me!](https://discord.com/api/oauth2/authorize?client_id=705135432588853288&permissions=8&scope=bot%20applications.commands)' })
-            .setTimestamp()
-            .setFooter({ text: 'Someone Bot By ApocalypseCalculator - Licensed', iconURL: client?.user?.avatarURL() ?? '' });
-
-        return interaction.reply({ embeds: [embed] });
+        return interaction.reply({
+            embeds: [createBaseEmbed(
+                client,
+                'Invite Me To Your Server!',
+                `[Click Me!](https://discord.com/api/oauth2/authorize?client_id=${client?.user?.id}&permissions=8&scope=bot%20applications.commands)`
+            )]
+        });
     },
 } as SlashCommand;
